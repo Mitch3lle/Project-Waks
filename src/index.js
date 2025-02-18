6958555d07db42afcd28bf703ca05675839d11af
Mitch3lle <wakwayamitchelle@gmail.com>
2025-02-18 09:46:55 +0300

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,33 @@ function refreshWeather(response){
     let temperatureElment = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+   // let description = response.data.condition.description;
+   let humidityElement = document.querySelector("#humidity");
+   let windSpeedElement = document.querySelector("#wind-speed");
+   let timeElement = document.querySelector("#time");
+   let date = new Date(response.data.time * 1000);
 
+    timeElement.innerHTML =  formatDate(date);
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    humidityElement.innerHTML =`${response.data.temperature.humidity}%`;
+    descriptionElement.innerHTML = response.data.condition.description;
     cityElement.innerHTML = response.data.city;
     temperatureElment.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date){
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes<10){
+        minutes = `0${minutes}`
+    }
+
+    return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city){
     let apiKey = "ef080d077be87f0o3fbae14a3eab95ta";
